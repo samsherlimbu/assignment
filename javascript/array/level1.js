fd7f970490f319cee128462767ffd6d954eb9f33
samsherlimbu <samshershigu6@gmail.com>
2024-04-23 09:17:37 +0545

    join rev

---
+++ javascript/array/level1.js
@@ -0,0 +1,16 @@
+const arr = [3,5,2]
+//Q1 calculate length of array and console log
+//expected output is 3
+const len = arr.length;
+console.log(len);
+
+//Q2 reverse the array and console log
+//expected output [2,5,3]
+const rev = arr.reverse();
+console.log(rev)
+
+
+///Q3 join the array and console log
+// expected output 352
+const joinn = rev.join('');
+console.log(joinn);
